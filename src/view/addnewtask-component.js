8caf965f908abd2e2e452efd2976162e9e2f731c
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-03-28 09:30:19 +0300

    Update addnewtask-component.js

--- src/view/addnewtask-component.js
+++ src/view/addnewtask-component.js
@@ -2,20 +2,22 @@ import {createElement} from '../framework/render.js';
 
 function createAddNewTaskComponentTemplate() {
     return (
-        `<form class="add-task__form" aria-label="Форма добавления задачи">
-        <div class="add-task__input-wrapper">
-        <label for="add-task">Новая задача</label>
-          <input type="text" name="task-name" id="add-task" placeholder="Название задачи..." required>
-        </div>
-        <button class="add-task__button button" type="submit">
-          <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="10.0833" y="3.66663" width="1.83333" height="14.6667" fill="white" />
-            <rect x="18.3333" y="10.0833" width="1.83333" height="14.6667" transform="rotate(90 18.3333 10.0833)"
-              fill="white" />
-          </svg>
-          <span>Добавить</span>
-        </button>
-      </form>`
+        `
+        <form class="add-task__form" aria-label="Форма добавления задачи">
+            <h1>Новая задача</h1>
+            <div class="bottom-group">
+                <input placeholder="Название задачи..." type="text">
+                <button>
+                    <svg width="15" height="15" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="10.0833" y="3.66663" width="1.83333" height="14.6667" fill="white" />
+                        <rect x="18.3333" y="10.0833" width="1.83333" height="14.6667" transform="rotate(90 18.3333 10.0833)"
+                        fill="white" />
+                    </svg>
+                    <span>Добавить</span>
+                </button>
+            </div>
+          </form>
+      `
       );
 }
 
