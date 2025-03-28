01f88d225984e128b038d505a398a9f3e5822b16
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-03-28 09:16:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,3 +12,12 @@ const boardContainer = document.querySelector('.board-app__main');
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(new AddNewTaskComponent(), formContainer);
 render(new TaskBoardComponent(), boardContainer);
+
+const taskListContainer = document.querySelector('.task-board');
+
+for (let i = 0; i < 4; i++) {
+    const list = new TasksListComponent();
+    render(list, taskListContainer);
+    const taskContainer = list.getElement().querySelector(".tasks");
+    for (let j = 0; j < 3; j++) { render(new TaskComponent(), taskContainer); }
+}
