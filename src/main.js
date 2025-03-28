d11be21e24a545d4c8b6c905c9b432fb88769d91
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-03-28 08:45:14 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import HeaderComponent from './view/header-component.js';
-import {render, RenderPosition} from './render.js';
+import {render, RenderPosition} from './framework/render.js';
 
 const bodyContainer= document.querySelector('.board-app');
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
