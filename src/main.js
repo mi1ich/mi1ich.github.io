cfefa68e972561bfcc0e23afa6f391e3563f8f9d
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-04-03 21:43:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import HeaderComponent from './view/header-component.js';
 import AddNewTaskComponent from './view/addnewtask-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import {render, RenderPosition} from './framework/render.js';
+import TasksModel from './model/tasks-model.js';
 
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.add-task');
 const tasksBoardContainer = document.querySelector('.board-app__main');
-const tasksBoardPresenter = new TasksBoardPresenter({boardContainer: tasksBoardContainer});
+
+const tasksModel = new TasksModel();
+const tasksBoardPresenter = new TasksBoardPresenter({
+  boardContainer: tasksBoardContainer,
+  tasksModel
+});
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(new AddNewTaskComponent(), formContainer);
