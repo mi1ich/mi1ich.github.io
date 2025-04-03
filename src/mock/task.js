e2b6928132da88e1c76909961d406f52c0e7fcc8
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-04-03 22:50:00 +0300

    Update task.js

--- src/mock/task.js
+++ src/mock/task.js
@@ -2,63 +2,19 @@ import { Status } from "../const.js";
 
 export const tasks = [
   {
-    id: "1",
-    title: "Сходить в магаз",
+    taskslist: ["Сходить в магаз", "Пойти погулять", "Устроиться на работу"],
     status: Status.BACKLOG
   },
   {
-    id: "2",
-    title: "Пойти погулять",
-    status: Status.BACKLOG
-  },
-  {
-    id: "3",
-    title: "Устроиться на работу",
-    status: Status.BACKLOG
-  },
-  {
-    id: "4",
-    title: "Выучить JS",
+    taskslist: ["Выучить JS", "Выучить React", "Создать приложение на Vue.js"],
     status: Status.PROCESSING
   },
   {
-    id: "5",
-    title: "Выучить React",
-    status: Status.PROCESSING
-  },
-  {
-    id: "6",
-    title: "Создать приложение на Vue.js",
-    status: Status.PROCESSING
-  },
-  {
-    id: "7",
-    title: "Разработать сервер на .NET (C#)",
+    taskslist: ["Разработать сервер на .NET (C#)", "Настроить маршрутизацию в React"],
     status: Status.DONE
   },
   {
-    id: "8",
-    title: "Настроить маршрутизацию в React",
-    status: Status.DONE
-  },
-  {
-    id: "9",
-    title: "Оптимизировать код на Vue.js",
-    status: Status.BASKET
-  },
-  {
-    id: "10",
-    title: "Удалить ненужные файлы",
-    status: Status.BASKET
-  },
-  {
-    id: "11",
-    title: "Очистить базу данных",
-    status: Status.BASKET
-  },
-  {
-    id: "12",
-    title: "Изучить архитектуру .NET Core",
+    taskslist: ["Оптимизировать код на Vue.js", "Удалить ненужные файлы", "Очистить базу данных", "Изучить архитектуру .NET Core"],
     status: Status.BASKET
   }
 ]
