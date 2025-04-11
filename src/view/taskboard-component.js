64c0b40c3dffde3b4d10f92f8f77c39affaaf032
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-04-11 10:31:11 +0300

    Update taskboard-component.js

--- src/view/taskboard-component.js
+++ src/view/taskboard-component.js
@@ -1,4 +1,4 @@
-import {createElement} from '../framework/render.js';
+import AbstractComponent from "../framework/view/abstract-component.js";
 
 function createTaskBoardTemplate() {
   return `
@@ -7,19 +7,8 @@ function createTaskBoardTemplate() {
   `;
 }
 
-export default class TaskBoardComponent {
-  getTemplate() {
+export default class TaskBoardComponent extends AbstractComponent {
+  get template() {
     return createTaskBoardTemplate();
-  }
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
-  }
-
-  removeElement() {
-    this.element = null;
   }
 }
