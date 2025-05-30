daeee7296871a50c1e7d4ef22617cb7958830241
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-05-30 12:33:15 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import AddNewTaskComponent from './view/addnewtask-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import TasksModel from './model/task-model.js';
 import ClearButtonComonent from './view/clearbutton-component.js';
+import TaskApiServices from './task-apiservices.js';
 
+const END_POINT = 'https://68396b016561b8d882b0502a.mockapi.io';
 const bodyContainer = document.querySelector('.page-body');
 const addTaskContainer = document.querySelector('.add-new-task-component');
 const deskContainer = document.querySelector('.main-content');
 
-
-const tasks = new TasksModel();
+const tasks = new TasksModel({
+    tasksApiServices: new TaskApiServices(END_POINT)
+});
 
 const clearButtonComponent = new ClearButtonComonent({
     onClick: handleClearBasketButtonClick
