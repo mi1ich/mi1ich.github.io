696d0c0bc5d1f047add359413cc34a6180524b5f
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-05-30 12:17:19 +0300

    Update task-component.js

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,18 +1,30 @@
 import AbstractComponent from "../framework/view/abstract-component.js";
 
-function createTaskTemplate(task) {
+function createTaskTemplate(id, task) {
     return (
-        `<li>${task}</li>`
+        `<li id=${id}>${task}</li>`
     );
 }
 
 export default class TaskComponent extends AbstractComponent {
-    constructor(tasks) {
+    constructor(task) {
         super();
-        this.tasks = tasks;
+        this.task = task;
+        this.#afterCreateElement();
     }
 
     get template() {
-        return createTaskTemplate(this.tasks);
+        return createTaskTemplate(this.task.id, this.task.name);
+    }
+
+    #afterCreateElement() {
+        this.#makeTaskDraggable();
+    }
+
+    #makeTaskDraggable() {
+        this.element.setAttribute('draggable', true);
+        this.element.addEventListener('dragstart', (evt) => {
+            evt.dataTransfer.setData('text/plain', this.task.id);
+        });
     }
 }
