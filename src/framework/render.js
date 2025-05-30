dc1215d6a99a0dec5657add9795ef615bb75bd65
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-05-30 11:38:23 +0300

    Update render.js

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,23 +1,29 @@
+import AbstractComponent from "./view/abstract-component.js";
+
 const RenderPosition = {
-  BEFOREBEGIN: 'beforebegin',
-  AFTERBEGIN: 'afterbegin',
-  BEFOREEND: 'beforeend',
-  AFTEREND: 'afterend',
+    BEFOREBEGIN: "beforebegin",
+    AFTERBEGIN: "afterbegin",
+    BEFOREEND: "beforeend",
+    AFTEREND: "afterend"
 };
 
-
 function createElement(template) {
-  const newElement = document.createElement('div');
-  newElement.innerHTML = template;
+    const newElement = document.createElement('div');
+    newElement.innerHTML = template;
 
-
-  return newElement.firstElementChild;
-}
-
+    return newElement.firstElementChild;
+};
 
 function render(component, container, place = RenderPosition.BEFOREEND) {
-container.insertAdjacentElement(place, component.getElement());
-}
+    if (!(component instanceof AbstractComponent)) {
+        throw new Error("Can render only components");
+    }
 
+    if (component === null) {
+        throw new Error("Container element doesn't exist");
+    }
 
-export {RenderPosition, createElement, render};
+    container.insertAdjacentElement(place, component.element);
+};
+
+export { RenderPosition, createElement, render };
