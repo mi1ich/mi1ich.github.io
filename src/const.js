1481cdd746cf4713b11f4f2114b812385e5b383d
mi1ich <168647370+mi1ich@users.noreply.github.com>
2025-05-30 12:32:43 +0300

    Update const.js

--- src/const.js
+++ src/const.js
@@ -19,4 +19,17 @@ const OrderPosition = {
     ABOVE: `above`
 }
 
-export {Status, StatusLabel, OrderPosition};
+const UserAction = {
+    UPDATE_TASK: 'UPDATE_TASK',
+    ADD_TASK: 'ADD_TASK',
+    DELETE_TASK: 'DELETE_TASK'
+};
+
+const UpdateType = {
+    PATCH: 'PATCH',
+    MINOR: 'MINOR',
+    MAJOR: 'MAJOR',
+    INIT: 'INIT'
+};
+
+export {Status, StatusLabel, OrderPosition, UserAction, UpdateType};
